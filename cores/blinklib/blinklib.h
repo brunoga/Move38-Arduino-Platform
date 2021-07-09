36fc24ecd8b2a178aaaf1e53e1ece405e2e6e82f
Bruno <bga@gmail.com>
2021-07-08 18:12:29 -0700

    Revert blinklib.* to Custom Blinklibn state.
    - Now Blik328 and BlinkMAX both seem to work as expected.
    - Hexenwood and Hexenwood AI compile without issues.

--- cores/blinklib/blinklib.h
+++ cores/blinklib/blinklib.h
@@ -3,15 +3,18 @@
  *
  * This defines a statefull view of the blinks tile interactions with neighbors.
  *
- * In this view, each tile has a "state" that is represented by a number between 1 and 127.
- * This state value is continuously broadcast on all of its faces.
- * Each tile also remembers the most recently received state value from he neighbor on each of its faces.
+ * In this view, each tile has a "state" that is represented by a number between
+ * 1 and 127. This state value is continuously broadcast on all of its faces.
+ * Each tile also remembers the most recently received state value from he
+ * neighbor on each of its faces.
  *
- * Note that this library depends on the blinklib library for communications with neighbors. The blinklib
- * IR read functions are not available when using the blinkstate library.
+ * Note that this library depends on the blinklib library for communications
+ * with neighbors. The blinklib IR read functions are not available when using
+ * the blinkstate library.
  *
- * Note that the beacon transmissions only occur when the loop() function returns, so it is important
- * that sketches using this model return from loop() frequently.
+ * Note that the beacon transmissions only occur when the loop() function
+ * returns, so it is important that sketches using this model return from loop()
+ * frequently.
  *
  */
 
@@ -32,92 +35,7 @@
 
 /*
 
-    IR communications functions
-
-*/
-
-// The value of the data sent and received on faces via IR can be between 0 and IR_DATA_VALUE_MAX
-// If you try to send higher than this, the max value will be sent.
-
-#define IR_DATA_VALUE_MAX 63
-
-// Returns the last received value on the indicated face
-// Between 0 and IR_DATA_VALUE_MAX inclusive
-// returns 0 if no neighbor ever seen on this face since power-up
-// so best to only use after checking if face is not expired first.
-
-byte getLastValueReceivedOnFace( byte face );
-
-// Did the neighborState value on this face change since the
-// last time we checked?
-
-// Note the a face expiring has no effect on the last value
-
-byte didValueOnFaceChange( byte face );
-
-// false if messages have been recently received on the indicated face
-// (currently configured to 100ms timeout in `expireDurration_ms` )
-
-byte isValueReceivedOnFaceExpired( byte face );
-
-// Returns false if their has been a neighbor seen recently on any face, returns true otherwise.
-bool isAlone();
-
-// Set value that will be continuously broadcast on specified face.
-// Value should be between 0 and IR_DATA_VALUE_MAX inclusive.
-// If a value greater than IR_DATA_VALUE_MAX is specified, IR_DATA_VALUE_MAX will be sent.
-// By default we power up with all faces sending the value 0.
-
-void setValueSentOnFace( byte value , byte face );
-
-// Same as setValueSentOnFace(), but sets all faces in one call.
-
-void setValueSentOnAllFaces( byte value );
-
-/* --- Datagram processing */
-
-// A datagram is a set of 1-IR_DATAGRAM_MAX_LEN bytes that are atomically sent over the IR link
-// The datagram is sent immediately on a best efforts basis. If it is not received by the other side then
-// it is lost forever. Each datagram sent is received at most 1 time. Once you have processed a received datagram
-// then you must mark it as read before you can receive the next one on that face. 
-
-// Must be smaller than IR_RX_PACKET_SIZE
-
-#define IR_DATAGRAM_LEN 16
-
-// Returns the number of bytes waiting in the data buffer, or 0 if no packet ready.
-byte getDatagramLengthOnFace( uint8_t face );
-
-// Returns true if a packet is available in the buffer
-boolean isDatagramReadyOnFace( uint8_t face );
-
- // Returns a pointer to the actual received datagram data
- // This should really be a (void *) so it can be assigned to any pointer type,
- // but in C++ you can not cast a (void *) into something else so it doesn't really work there
- // and I think too ugly to have these functions that are inverses of each other to take/return different types.
- // Thanks, Stroustrup.
-const byte *getDatagramOnFace( uint8_t face );
-
-// Frees up the buffer holding the datagram data. Do this as soon as possible after you have
-// processed the datagram to free up the slot for the next incoming datagram on this face.
-// If a new datagram is recieved on a face before markDatagramReadOnFace() is called then
-// the new datagram is siliently discarded. 
-
-void markDatagramReadOnFace( uint8_t face );
-
-// Send a datagram.  
-// Datagram is sent as soon as possible and takes priority over sending a value on face.
-// If you call sendDatagramOnFace() and there is already a pending datagram, the older pending
-// one will be replaced with the new one. 
-
-// Note that if the len>IR_DATAGRAM_LEN then packet will never be sent or recieved
-
-void sendDatagramOnFace(  const void *data, byte len , byte face );
-
-
-/*
-
-	This set of functions let you test for changes in the environment.
+        This set of functions let you test for changes in the environment.
 
 */
 
@@ -156,140 +74,18 @@ bool buttonDoubleClicked();
 
 bool buttonMultiClicked();
 
-
-// The number of clicks in the longest consecutive valid click cycle since the last time called.
-byte buttonClickCount(void);
+// The number of clicks in the longest consecutive valid click cycle since the
+// last time called.
+byte buttonClickCount();
 
 // Remember that a long press fires while the button is still down
 bool buttonLongPressed();
 
-// 6 second press. Note that this will trigger seed mode if the blink is alone so
-// you will only ever see this if blink has neighbors when the button hits the 6 second mark.
-// Remember that a long press fires while the button is still down
-bool buttonLongLongPressed(void);
-
-/*
-
-	This set of functions lets you control the colors on the face RGB LEDs
-
-*/
-
-
-// Ok, it kills me to include this #include here because it pulls all of these symbols into
-// the Arduino namespace... but, dude, Arduino pulls in everything anyway!!!!
-// To do it right we'd break out the only thing we actually want (pixelColor_t) into
-// its own header file and only include that. Maybe someday.
-// TODO: Break pixelColor_t into blinkbios_shared_pixelcolor_t.h just for neatness counts
-
-#include "shared/blinkbios_shared_pixel.h"
-
-typedef pixelColor_t Color;
-
-// Number of visible brightness levels in each channel of a color
-#define BRIGHTNESS_LEVELS_5BIT 32
-#define MAX_BRIGHTNESS_5BIT    (BRIGHTNESS_LEVELS_5BIT-1)
-
-// R,G,B are all in the domain 0-31
-// Here we expose the internal color representation, but it is worth it
-// to get the performance and size benefits of static compilation
-// Shame no way to do this right in C/C++
-
-#define MAKECOLOR_5BIT_RGB(r,g,b) (pixelColor_t(r,g,b,1))
-
-#define RED         MAKECOLOR_5BIT_RGB(MAX_BRIGHTNESS_5BIT, 0                    ,0)
-#define ORANGE      MAKECOLOR_5BIT_RGB(MAX_BRIGHTNESS_5BIT,MAX_BRIGHTNESS_5BIT/2 ,0)
-#define YELLOW      MAKECOLOR_5BIT_RGB(MAX_BRIGHTNESS_5BIT,MAX_BRIGHTNESS_5BIT   ,0)
-#define GREEN       MAKECOLOR_5BIT_RGB( 0                 ,MAX_BRIGHTNESS_5BIT   ,0)
-#define CYAN        MAKECOLOR_5BIT_RGB( 0                 ,MAX_BRIGHTNESS_5BIT   ,MAX_BRIGHTNESS_5BIT)
-#define BLUE        MAKECOLOR_5BIT_RGB( 0                 , 0                    ,MAX_BRIGHTNESS_5BIT)
-#define MAGENTA     MAKECOLOR_5BIT_RGB(MAX_BRIGHTNESS_5BIT, 0                    ,MAX_BRIGHTNESS_5BIT)
-
-#define WHITE       MAKECOLOR_5BIT_RGB(MAX_BRIGHTNESS_5BIT,MAX_BRIGHTNESS_5BIT   ,MAX_BRIGHTNESS_5BIT)
-
-#define OFF         MAKECOLOR_5BIT_RGB( 0                 , 0                    , 0)
-
-#define GET_5BIT_R(color) (color.r)
-#define GET_5BIT_G(color) (color.g)
-#define GET_5BIT_B(color) (color.b)
-
-// Dim the specified color. Brightness is 0-255 (0=off, 255=don't dim at all-keep original color)
-
-#define MAX_BRIGHTNESS (255)
-
-Color dim( Color color, byte brightness);
-
-// Brighten the specified color. Brightness is 0-255(0=unaltered color, 255=full white)
-
-Color lighten( Color color, byte brightness);
-
-// This maps 0-255 values to 0-31 values with the special case that 0 (in 0-255) is the only value that maps to 0 (in 0-31)
-// This leads to some slight non-linearity since there are not a uniform integral number of 1-255 values
-// to map to each of the 1-31 values.
-
-// Make a new color from RGB values. Each value can be 0-255.
-
-Color makeColorRGB( byte red, byte green, byte blue );
-
-// Make a new color in the HSB colorspace. All values are 0-255.
-
-Color makeColorHSB( byte hue, byte saturation, byte brightness );
-
-// Change the tile to the specified color
-// NOTE: all color changes are double buffered
-// and the display is updated when loop() returns
-
-void setColor( Color newColor);
-
-// Set the pixel on the specified face (0-5) to the specified color
-// NOTE: all color changes are double buffered
-// and the display is updated when loop() returns
-
-void setColorOnFace( Color newColor , byte face );
-
-// DEPREICATED: Use setColorOnFace()
-//void setFaceColor( byte face , Color newColor ) __attribute__ ((deprecated));
-void setFaceColor(  byte face, Color newColor );
-
-/*
-
-    Timing functions
-
-*/
-
-// Number of running milliseconds since power up.
-//
-// Important notes:
-// 1) does not increment while sleeping
-// 2) is only updated between loop() interations
-// 3) is not monotonic, so always use greater than
-//    and less than rather than equals for comparisons
-// 4) overflows after about 50 days
-// 5) is only accurate to about +/-10%
-
-unsigned long millis(void);
-
-class Timer {
-
-	private:
-
-		uint32_t m_expireTime;		// When this timer will expire
-
-	public:
-
-		Timer() {};		                    // Timers come into this world pre-expired.
-
-		bool isExpired();
-
-        uint32_t getRemaining();
-
-        void set( uint32_t ms );            // This time will expire ms milliseconds from now
-
-		void add( uint16_t ms );
-
-        void never(void);                   // Make this timer never expire (unless set())
-
-};
-
+// 6 second press. Note that this will trigger seed mode if the blink is alone
+// so you will only ever see this if blink has neighbors when the button hits
+// the 6 second mark. Remember that a long press fires while the button is still
+// down
+bool buttonLongLongPressed();
 
 /*
 
@@ -365,23 +161,24 @@ bool hasWoken();
 
 byte startState(void);
 
-// Make the current game sterile so that it can not be propagated to other blinks (niche) 
-// If sterileFlag==1 then holding the button down will never enter seed mode, it will just eventually sleep. 
-// This essentially means that once this built-in game is programmed into a blink it will not transfer to other blinks.
+// Make the current game sterile so that it can not be propagated to other
+// blinks (niche) If sterileFlag==1 then holding the button down will never
+// enter seed mode, it will just eventually sleep. This essentially means that
+// once this built-in game is programmed into a blink it will not transfer to
+// other blinks.
 
-// There are two ways you can use this. If you statically want a game to be sterile, you can add...
-// uint8_t sterileFlag=1;             // Make this game sterile. 
-// outside of any function block. This will completely disable all seeding and also give you some
-// extra flash memory since the seeding code does not even get linked into the executable (thanks gcc LTO!)
-// Alternately, you can use..
-// sterileFlag=1;
+// There are two ways you can use this. If you statically want a game to be
+// sterile, you can add... uint8_t sterileFlag=1;             // Make this game
+// sterile. outside of any function block. This will completely disable all
+// seeding and also give you some extra flash memory since the seeding code does
+// not even get linked into the executable (thanks gcc LTO!) Alternately, you
+// can use.. sterileFlag=1;
 // ...and...
 // sterileFlag=0;
 // inside your code to dynamically enable and disable seeding at any time.
-// NB: setting sterileFlag only suppress button-initiated seeds. 
+// NB: setting sterileFlag only suppress button-initiated seeds.
 
-extern uint8_t sterileFlag;             // Set to 1 to make this game sterile. 
-
+extern uint8_t sterileFlag;  // Set to 1 to make this game sterile.
 
 /*
 
